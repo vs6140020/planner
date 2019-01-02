2ad0c85c7c9a9198375c0e058264e229bce596cd
vs6140020 <vs6140020@outlook.com>
2019-01-02 16:07:13 +0530

    Further changes linking home page and template page.

--- plan/views.py
+++ plan/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Project, Task, Subtask
+from django.shortcuts import render
 
 # Create your views here.
+def default(request):
+	project_list = Project.objects.all()
+	context = {
+		'project_list' : project_list,
+	}
+	return render(request, 'plan/home.html', context)
+
+def template(request, project_name):
+	task_list = Project.objects.get(project_name = project_name).task_set.all()
+	context = {
+		'task_list' : task_list,
+	}
+	return render(request, 'plan/template.html', context)
